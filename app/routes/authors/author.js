44d246a567f60e0ba3c887e1bb11f74eb458deed
Jaime Mejia <jdmejiaq@unal.edu.co>
2018-05-11 12:10:45 -0500

    trabajo

--- app/routes/authors/author.js
+++ app/routes/authors/author.js
@@ -1,4 +1,22 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+
+  model({id}){
+    const author = this.modelFor('authors');
+    const authors = author.findBy('id', id);
+    const posts = author.get('posts');
+    const authorName = authors.get('userName');
+
+    if(!authors){
+      this.transitionTo('authors');
+    }else {
+      return{
+        authors,
+        posts,
+        authorName,
+
+      };
+    }
+  }
 });
